b75ec48310da94f4b32514ee363e0bed4f279a6f
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2023-12-19 16:28:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import { Alert } from "react-bootstrap/Alert";
+// import Alert from "react-bootstrap/Alert";
+import MyNav from "./component/componentNav";
+import MyFooter from "./component/componentFooter";
+import MyCard from "./component/myCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <MyNav />
       </header>
+      <main>
+        <MyCard />
+      </main>
+      <footer>
+        <MyFooter />
+      </footer>
     </div>
   );
 }
